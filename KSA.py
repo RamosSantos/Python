bfb054171edf4b434d02799ff55ec4245a7f304c
Felipe <f.ramossantos@gmail.com>
2014-05-02 16:52:28 -0300

    First Commit

--- KSA.py
+++ KSA.py
@@ -1,6 +1,7 @@
 '''
                 ### Script that generate a key KSA###
-
+            Implentation of the algorithm by Felipe Ramos
+    
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
     to deal in the Software without restriction, including without limitation
@@ -30,8 +31,6 @@ def KSA(key):
         s[i],s[j]=s[j],s[i]
     return s
  
-
-       
 
 if __name__=="__main__":
     key=input("Input your key: ")
